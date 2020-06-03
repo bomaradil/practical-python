933a53eb90c395a6cc3ad07e214a73d3669db085
bomaradil <56771009+bomaradil@users.noreply.github.com>
2020-06-03 21:19:55 +0100

    commit change for 48h work (exercise 2.7)

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,26 @@
 # pcost.py
 #
 # Exercise 1.27
+import csv, sys
+
+def portfolio(filename):
+    with open(filename) as f:
+        next(f)
+        cost = 0
+        
+        for line in csv.reader(f):
+            #line = line.split(',')
+            try:
+                cost += float(line[-1]) * int(line[-2])
+            except ValueError as err:
+                print('Warning :', err)
+        
+    return cost
+
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+else:
+    filename = 'Data/portfolio.csv'
+
+cost = portfolio(filename)
+print('Total cost: ', cost)
--- Work/report.py
+++ Work/report.py
@@ -1,3 +1,31 @@
 # report.py
 #
 # Exercise 2.4
+
+import csv
+
+portfolio = []
+#share = {}
+def read_portfolio(filename):
+    with open(filename) as f:
+        next(f)
+        for row in csv.reader(f):
+            #holding = (row[0], int(row[1]), float(row[2]))
+            share = {'name' : row[0], 'shares' : int(row[1]), 'price' : float(row[2])}
+            portfolio.append(share)
+    
+    return portfolio
+
+def read_prices(filename):
+    with open(filename) as f:
+        prices = {}
+        for row in csv.reader(f):
+           # try:
+           #     prices[row[0]] = float(row[1])
+           # except IndexError as err:
+           #     print('warning empty block', err)
+           if row:
+               prices[row[0]] = float(row[1])
+        return prices
+    
+def lose
