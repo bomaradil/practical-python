b335bce79dc9e970d5028c73d387b83fce38ad68
bomaradil <56771009+bomaradil@users.noreply.github.com>
2020-06-07 21:19:43 +0100

    creating and scripting file pileparse.py

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -1,3 +1,69 @@
 # fileparse.py
 #
 # Exercise 3.3
+
+import csv
+
+records = []
+
+
+def parse_csv(filename, select=None, types=None, has_headers=True, delimiter=',', silence_errors=False):
+    '''
+    parse a csv file into a list of records
+    '''
+    if select and has_headers == False:  
+        # raising an expection if select=true and has_headers=False
+        raise RuntimeError("select arguement requires column headers")
+
+    with open(filename) as f:
+        rows = csv.reader(f, delimiter=delimiter)
+
+        if has_headers:     # if the csv file has a headers
+            headers = next(rows)  # read the file headers
+
+            if select:  # if a colum select was given
+                # making sure that elements in select exit in the headers
+                for c in range(len(select)):
+                    if select[c] in headers:
+                        continue
+                    else:
+                        print('select element are not in the headers of csv file')
+                        break
+                headers = select
+            # mapping the colum selection to indices
+                indices = [headers.index(colname) for colname in select]
+            else:
+                indices = []
+
+            for lineno, row in enumerate(rows, start=1):
+                if not row:  # skip empty lines
+                    continue
+                if indices:  # filter the row if specific colums were selected
+                    row = [row[index] for index in indices]
+
+                if types:  # marking the type of values
+                    try:
+                        row = [func(val) for func, val in zip(types, row)]
+                    except ValueError as err:
+                        if silence_errors == False:
+                            print(f'row {lineno}: couldn\'t convert {row}')
+                            print(f'row {lineno}: {err}')
+                            continue
+                        else:
+                            continue
+                        
+                record = dict(zip(headers, row))
+                records.append(record)
+
+        else:
+            # if no headers in the csv file
+            for row in rows:
+                if not row:  # skip empty lines
+                    continue
+                if types:  # marking the type of values
+                    row = [func(val) for func, val in zip(types, row)]
+
+                record = (row[0], row[1])
+                records.append(record)
+
+        return records
--- Work/report.py
+++ Work/report.py
@@ -9,6 +9,9 @@ prices = {}
 report = []
 
 def read_portfolio(filename):
+    '''
+    create a dictionary inside a list from a csv file using a loop 
+    '''
     with open(filename) as f:
         rows = csv.reader(f)
         headers = next(rows)
@@ -18,9 +21,23 @@ def read_portfolio(filename):
             portfolio.append(share)
     return portfolio
 
-def read_prices(filename):
+def read_portfolio_2(filename):
+    '''
+    create a dictionary inside a list from a csv file using a loop in one line
+    '''
     with open(filename) as f:
-        
+        rows = csv.reader(f)
+        headers = next(rows)
+        select = ['name', 'shares', 'price']
+        indices = [ headers.index(colname) for colname in select ]
+        portfolio = [ { colname: row[index] for colname, index in zip(select, indices) } for row in rows ]
+    return portfolio
+
+def read_prices(filename):
+    '''
+    create a list from a csv file
+    '''
+    with open(filename) as f:
         for row in csv.reader(f):
            # try:
            #     prices[row[0]] = float(row[1])
@@ -31,11 +48,14 @@ def read_prices(filename):
         return prices
         
 def loss_gain():
+    '''
+    comparing the share price with the portfolio price and return if we make a gain or loss
+    '''
     total_price = 0
     price_share = 0
-    for i in portfolio:
-        total_price += prices[i['name']] * i['shares']
-        price_share += i['shares'] * i['price']
+    #for i in portfolio:
+    total_price = sum([prices[i['name']] * i['shares']] for i in portfolio)
+    price_share = sum([i['shares'] * i['price']] for i in portfolio)
         
     if total_price > price_share:
         print('Your losses is: ', round(total_price - price_share, 2))
@@ -43,17 +63,34 @@ def loss_gain():
         print('your gain is: ', round(price_share - total_price, 2))
 
 def make_report(portfolio, prices):
+    '''
+    combining the info in the csv portfolio and price file 
+    and making a list report for the name, total, price and change of the share 
+    '''
     for i in portfolio:
-        change = float(prices[i['name']] - i['price'])
-        report.append((i['name'], i['shares'], prices[i['name']], round(change, 2)))
+        change = float(prices[i['name']]) - float(i['price'])
+        report.append((i['name'], int(i['shares']), prices[i['name']], round(change, 2)))
     return report
 
 def print_report(report):
+    '''
+    printing the report 
+    '''
     headers = ('Name', 'Shares', 'Price', 'Change')
     print('%10s %10s %10s %10s' % headers)
     print('---------- ----------- ----------- -----------')
     for name, shares, price, change in report:
         print(f'{name:>10s} {shares:>10d} {"$":>6s}{price:<7.2f} {change:>10.2f}')
+
+
+def portfolio_report(portfolio_filename, price_filename):
+    '''
+    operating the script
+    '''
+    read_portfolio(portfolio_filename)
+    read_prices(price_filename)
+    make_report(portfolio, prices)
+    print_report(report)
 
 #portfolio = read_portfolio('Data/portfolio.csv')
 #prices = read_prices('Data/prices.csv')
